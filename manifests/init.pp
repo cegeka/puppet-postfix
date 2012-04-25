00e79ebd0daf0670892cde279028b6c56f9c0107
Kristof Willaert <kristof.willaert@gmail.com>
2012-04-25 16:49:16 +0200

    Initial repository layout for puppet module postfix

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: postfix
+#
+# This module manages postfix
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class postfix {
+
+
+}
