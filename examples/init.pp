2e943615fb11552e971b202277364132f9466571
fdammeke <fabian.dammekens@cegeka.be>
2012-11-02 16:11:26 +0100

    Added more generic funtionality

--- examples/init.pp
+++ examples/init.pp
@@ -1,6 +1,12 @@
 class { 'postfix':
-  relayer           => 'dummy.isp.tld',
+  #  relayer           => 'dummy.isp.tld',
+  relayer           => 'smtp.cegeka.be',
 }
-postfix::postmapfile{ 'recipient_access':
-  options => ['dummy.intra REJECT Delivery to this domain is prohibited.']
+#postfix::postmapfile{ 'recipient_access':
+#  options => ['dummy.intra REJECT Delivery to this domain is prohibited.']
+#}
+
+postfix::postmapfile{ 'virtual':
+  #  options => ['dummy.isp.tld forward@other.isp.tld'],
+  options => ['telenet.be fabian.dammekens@cegeka.be'],
 }
